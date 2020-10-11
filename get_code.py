d125c9bfdef93db18ff076c88c50a05a25546fb8
wwang33 <wwang33@ncsu.edu>
2020-10-11 14:01:39 -0400

    fall20 data analysis

--- get_code.py
+++ get_code.py
@@ -4,18 +4,23 @@ from save_load_pickle import *
 from datetime import datetime
 
 trace = pd.read_csv("../trace_data/trace.csv")
-trace = trace.sort_values(by=['userID', 'time'])
-trace = trace[trace.assignmentID=='day2']
+trace = trace.sort_values(by=['assignmentID', 'userID', 'time'])
+trace = trace[trace.assignmentID.isin(['day2', 'day3'])]
 
-
+teamID_reversed = {}
+for i in userID_teamID_maps:
+    teamID_reversed[userID_teamID_maps[i]] = i
 
 def trace2code(trace):
-    code_data = pd.DataFrame(columns=['teamID', 'code'])
+    code_data = pd.DataFrame(columns=['assignmentID', 'teamID', 'code', 'time', "projectID"])
     old_team_id = ''
     for i in tqdm(trace.index):
         if not isinstance(trace.at[i, 'code'], str):
             continue
         user_id = trace.at[i, 'userID']
+        assignment_id = trace.at[i, 'assignmentID']
+        project_id = trace.at[i, 'projectID']
+        time = trace.at[i, 'time']
         try:
             team_id = userID_teamID_maps[user_id]
         except:
@@ -23,27 +28,40 @@ def trace2code(trace):
         if team_id != old_team_id:
             print("team_id: ", team_id)
             if old_team_id:
-                new_row = {"teamID": old_team_id, "code": code}
+                new_row = {"teamID": old_team_id, "assignmentID" : assignment_id, "code": code, "time": time, "projectID": project_id}
                 code_data.loc[len(code_data)] = new_row
             old_team_id = team_id
         code = trace.at[i, 'code']
 
-    code_data = code_data.sort_values(by=['teamID'])
+    code_data = code_data.sort_values(by=['assignmentID', 'teamID'])
     save_obj(code_data, 'code_data', 'results')
 
-def code2xml(code_data):
+def code2xml_with_name(code_data):
     combined_code_data = pd.DataFrame(columns = ['combinedID', 'code'])
     old_combined_team_id = ''
     for i in code_data.index:
         team_id = code_data.at[i, 'teamID']
-        combined_team_id = team_id[0:3]
+        print("team_id", team_id)
+        # team_id = team_id[:3]
+        # written_id = teamID_reversed[team_id + "_0"]
+        written_id = teamID_reversed[team_id]
+        written_id0 = written_id.split("/")[0]
+        written_id1 = written_id.split("/")[1]
+        final_id = written_id0 + "_" + written_id1
+
+        combined_team_id = code_data.at[i, 'assignmentID'] + "_"+ final_id
         if combined_team_id != old_combined_team_id:
             if old_combined_team_id:
                 new_row = {"combinedID": combined_team_id, "code": code}
                 combined_code_data.loc[len(combined_code_data)]= new_row
             old_combined_team_id = combined_team_id
         code = code_data.at[i, 'code']
-    save_obj(combined_code_data, 'combined_code_data', 'results')
+        f = open("results/xml_with_name_new/" + combined_team_id + ".xml", "w")
+        f.write(code)
+        f.close()
 
+    save_obj(combined_code_data, 'combined_code_data_with_name', 'results')
+
+trace2code(trace)
 code_data = load_obj("code_data", 'results')
-code2xml(code_data)
+code2xml_with_name(code_data)
--- log_analysis.py
+++ log_analysis.py
@@ -1,53 +1,9 @@
 import pandas as pd
-from student_group_maps import *
 from save_load_pickle import *
 from datetime import datetime, date, time
 
-trace = pd.read_csv("../trace_data/trace.csv")
-trace = trace.sort_values(by=['userID', 'time'])
-trace = trace[trace.assignmentID.isin(['day2', 'day3'])]
+trace = pd.read_csv("trace_data/trace.csv")
 
-
-def trace2code(trace):
-    code_data = pd.DataFrame(columns=['teamID', 'code'])
-    old_team_id = ''
-    for i in tqdm(trace.index):
-        if not isinstance(trace.at[i, 'code'], str):
-            continue
-        user_id = trace.at[i, 'userID']
-        try:
-            team_id = userID_teamID_maps[user_id]
-        except:
-            continue
-        # if team_id.split("_")[0] == "b":
-            # print(trace.loc[i])
-        if team_id != old_team_id:
-            if old_team_id:
-                new_row = {"teamID": old_team_id, "code": code}
-                code_data.loc[len(code_data)] = new_row
-            old_team_id = team_id
-        code = trace.at[i, 'code']
-
-    code_data = code_data.sort_values(by=['teamID'])
-    save_obj(code_data, 'code_data', 'results')
-
-
-trace2code(trace)
-
-# trace.head()
-
-teams = ["a_1_0", "a_1_1", "a_2_0", "a_2_1", "a_3_0", "a_3_1", "a_4_0", "a_4_1", "a_5_0", "a_5_1",
-         "a_6_0", "a_6_1", "a_7_0", "a_7_1", "b_1_0", "b_1_1", "b_2_0", "b_2_1", "b_3_0", "b_3_1", "b_4_0",
-         "b_4_1", "b_5_0", "b_5_1", "b_6_0", "b_6_1"]
-
-
-# teams = ["a_1_0", "a_1_1"]
-
-messageDict = {
-    'EBFSummer20.iSnap.ExampleDialog.popUp', 'EBFSummer20.iSnap.ExampleDialog.Close.By.gifClick',
-    'EBFSummer20.iSnap.ExampleDialog.Close.By.okClick', 'EBFSummer20.iSnap.ExampleDialog.Close.By.Gif.Overlay.crossClick',
-    'EBFSummer20.iSnap.Gallery.Overlay.close', 'ExampleFeedback.logFeedback', 'EBFSummer20.iSnap.ExampleDialog.selfExplation.InputSlot.editted'
-}
 
 whenClosed = {'EBFSummer20.iSnap.ExampleDialog.Close.By.gifClick', 'EBFSummer20.iSnap.Gallery.Overlay.close',
               'EBFSummer20.iSnap.ExampleDialog.Close.By.okClick'}
@@ -55,107 +11,71 @@ whenClosed = {'EBFSummer20.iSnap.ExampleDialog.Close.By.gifClick', 'EBFSummer20.
 trivialExp = {"\n", "Explanation...\n\n\n", "Explanation...\n\n\nT"}
 
 
-finalResultsSkeleton = {'teamID': None, 'userID': None, 'gifName': None, 'timeOnExample': None, 'selfExplanation': None, 'openingTime': None, 'closingTime': None}
-
-
-
 # Functions and other data declared/defined here
-def checkLogFeedBackConditions(result, i):
-    if result.at[i, "message"] == 'ExampleFeedback.logFeedback' and eval(result.at[i, "data"])['explanation'] not in trivialExp: return True
+def check_feedback_conditions(result, i):
+    if result.at[i, "message"] == 'ExampleFeedback.logFeedback' and eval(result.at[i, "data"])[
+        'explanation'] not in trivialExp:
+        return True
     return False
 
-def mapper(id):
-    return userID_teamID_maps[id]
 
 def getTimeDifference(start, end):
     # This is a dummy date since we do not need the date stamp as all of our results are for day2 currently.
     # But it should be changed if need be
     _date = date(2005, 7, 14)
-    datetime1 = datetime.combine(_date, pd.to_datetime(result.loc[[start]].time).dt.time[start])
-    datetime2 = datetime.combine(_date, pd.to_datetime(result.loc[[end]].time).dt.time[end])
+    datetime1 = datetime.combine(_date, pd.to_datetime(trace.loc[[start]].time).dt.time[start])
+    datetime2 = datetime.combine(_date, pd.to_datetime(trace.loc[[end]].time).dt.time[end])
     return (datetime2 - datetime1).total_seconds()
 
-finalResults = pd.DataFrame(columns=['teamID', 'gifName', 'timeOnExample', 'selfExplanation','openingTime', 'closingTime'])
 
 def processData(start, end, explanation):
-    finalResultsSkeleton['userID'] = result.at[start, "teamID"]
-    finalResultsSkeleton['teamID'] = result.at[start, "teamID"][:3]
-    finalResultsSkeleton['gifName'] = result.at[start, "data"]
-    finalResultsSkeleton['timeOnExample'] = getTimeDifference(start, end)
-    finalResultsSkeleton['selfExplanation'] = explanation.replace('Explanation...\n\n\n', '')
-    finalResultsSkeleton['openingTime'] = result.at[start, "time"]
-    finalResultsSkeleton['closingTime'] = result.at[end, "time"]
-    return finalResults.append(finalResultsSkeleton, ignore_index=True)
-
-out = trace[trace['userID'].isin(userID_teamID_maps)].reset_index()
-del out['index']
-del out['sessionID']
-del out['browserID']
-del out['serverTime']
-del out['id']
-del out['projectID']
-
-out = out[out['message'].isin(messageDict)].reset_index(drop = True)
-
-teamId = list(map(mapper, out['userID']))
-out.insert(loc=0, column='teamID', value=teamId)
-del out['userID']
-
-for team in teams:
-
-    result = out[:]
-    result = result.reset_index(drop=True)
-    result = result.loc[result['teamID'].isin([team])].reset_index(drop=True)
-    result = result.sort_values(by=['time']).reset_index(drop=True)
-
-    i, start = 0, 0
-    while i < result.shape[0]:
-        start = i
-        if result.at[i, "message"] == 'EBFSummer20.iSnap.ExampleDialog.popUp':
-            i += 1
-            exp = 'No Explanation'
-            end = 0
-            while i < result.shape[0] and result.at[i, "message"] != 'EBFSummer20.iSnap.ExampleDialog.popUp':
-                if checkLogFeedBackConditions(result, i):  exp = eval(result.at[i, "data"])['explanation']
-                if result.at[i, "message"] in whenClosed and not end: end = i
-                i += 1
-            finalResults = processData(start, end, exp)
-        else:
-            i += 1
+    new_row = {}
+    new_row['userID'] = trace.at[start, "userID"]
+    new_row['projectID'] = trace.at[start, "projectID"]
+    new_row['gifName'] = trace.at[start, "data"]
+    new_row['timeOnExample'] = getTimeDifference(start, end)
+    new_row['selfExplanation'] = explanation.replace('Explanation...\n\n\n', '')
+    new_row['openingTime'] = trace.at[start, "time"]
+    new_row['closingTime'] = trace.at[end, "time"]
+    return new_row
 
 
+result_data = pd.DataFrame(
+    columns=['projectID', 'gifName', 'timeOnExample', 'selfExplanation', 'openingTime', 'closingTime'])
 
-right, left = 0, 0
-i, start = 0, 0
 
-whenClosed = {'EBFSummer20.iSnap.ExampleDialog.Close.By.gifClick', 'EBFSummer20.iSnap.Gallery.Overlay.close',
-              'EBFSummer20.iSnap.ExampleDialog.Close.By.okClick'}
+trace.userID = trace.userID.apply(lambda x: x.split("[DELIM]")[0])
+trace = trace.sort_values(by=['userID', 'time'])
 
-trivialExp = {"\n", "Explanation...\n\n\n", "Explanation...\n\n\nT"}
+user_ids = trace.userID.unique()
 
-while i < result.shape[0]:
 
-    start = i
-    if result.at[i, "message"] == 'EBFSummer20.iSnap.ExampleDialog.popUp':
-        i += 1
-        exp = 'No Explanation'
-        end = 0
-        while i < result.shape[0] and result.at[i, "message"] != 'EBFSummer20.iSnap.ExampleDialog.popUp':
+# for i in trace.index:
+#     if trace.at[i, 'message'] == 'EBFSummer20.iSnap.ExampleDialog.popUp':
+#         start = i
+#         end = 0
+#         exp = ""
+#         if i < trace.shape[0] and trace.at[i, "message"] != 'EBFSummer20.iSnap.ExampleDialog.popUp':
+#             if check_feedback_conditions(trace, i):
+#                 exp = eval(trace.at[i, "data"])['explanation']
+#             if trace.at[i, "message"] in whenClosed and not end:
+#                 end = i
+#         new_row = processData(start, end, exp)
+#         result_data = result_data.append(new_row, ignore_index=True)
 
-            if result.at[i, "message"] == 'ExampleFeedback.logFeedback' and eval(result.at[i, "data"])[
-                'explanation'] not in trivialExp:
-                exp = eval(result.at[i, "data"])['explanation']
-                # print(eval(result.at[i, "data"])['explanation'])
+# print(result_data)
 
-            if result.at[i, "message"] in whenClosed and not end:
-                end = i
 
-            i += 1
+for user_id in user_ids:
+    sub_trace = trace[trace.userID.eq(user_id)]
+    gif_name_set = {}
+    for i in sub_trace.index:
+        if sub_trace.at[i, 'message'] == 'EBFSummer20.iSnap.ExampleDialog.popUp':
+            gif_name_set.add()
+    print(sub_trace)
 
-        #         end = i-1
-        finalResults = processData(start, end, exp)
-    else:
-        i += 1
 
-print(finalResults)
-save_obj(finalResults, "final_results", "results")
+
+
+
+save_obj(result_data, 'result_data', 'results')
